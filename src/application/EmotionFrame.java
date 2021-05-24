ef9a326fafe6aca49d0f1366738763ca01062c13
Lee Jihyeon <gee9328@naver.com>
2021-05-24 14:54:57 +0900

    Mood page : 버튼 수정, DB 생성, change mood 버튼 생성

--- src/application/EmotionFrame.java
+++ src/application/EmotionFrame.java
@@ -1,148 +1,185 @@
 package application;
 
-import java.awt.BorderLayout;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.io.File;
+
 import java.io.IOException;
-import java.util.Calendar;
-import java.awt.GridLayout;
+
+import java.io.FileWriter;
+import java.io.File;
+
+import java.io.BufferedWriter;
+
 import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextPane;
-import javax.swing.border.Border;
-import javax.swing.text.SimpleAttributeSet;
-import javax.swing.text.StyleConstants;
-import javax.swing.text.StyledDocument;
-import javax.swing.text.html.HTMLDocument;
-import javax.swing.text.html.HTMLEditorKit;
+
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import java.util.*;
+
 
 public class EmotionFrame extends JFrame {
 	
-	// int selectMood = -1;
-	
-	public EmotionFrame() {
-		Font font = new Font("占쏙옙占쏙옙占쏙옙占�", Font.PLAIN, 20);
-		JFrame frm = new JFrame("Emotion");
-		frm.setSize(500, 400);
-		frm.setLocationRelativeTo(null);
-		frm.getContentPane().setLayout(null);
-		frm.setResizable(false);
-		frm.getContentPane().setBackground(Color.WHITE);
-		JLabel lab = new JLabel();
-		lab.setBounds(170,20,500,50);
-		lab.setText("Emotion selection");
-		lab.setFont(font);
-		frm.getContentPane().add(lab);
-		JButton [] btn = {new JButton(), new JButton(), new JButton(), new JButton(), new JButton(), new JButton()};
-		int x = 75;
-		int y = 100;
-		for(int i = 0; i < btn.length; i++) {
-		btn[i].setBounds(x, y, 100, 100);
-			btn[i].setBorderPainted(false);
-			btn[i].setContentAreaFilled(false);
-			frm.getContentPane().add(btn[i]);
-			x += 130;
-			if(i % 3 == 2) {
-				y += 120;
-				x = 75;
-			}
-		}
-		ImageIcon[] img= {new ImageIcon("./data/images/mood/happy.png"), new ImageIcon("./data/images/mood/good.png"), new ImageIcon("./data/images/mood/tired.png"),
-						  new ImageIcon("./data/images/mood/bad.png"), new ImageIcon("./data/images/mood/sad.png"), new ImageIcon("./data/images/mood/sick.png")};
+	public EmotionFrame(String day) {
 		
-		for(int i = 0; i < img.length; i++) {
-			Image temp = img[i].getImage();
-			Image change = temp.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
-			ImageIcon tempicon = new ImageIcon(change);
-			btn[i].setIcon(tempicon);
-		}
-		
-		// happy
-		btn[0].addActionListener(new ActionListener() {
+		Font font = new Font("나눔 고딕", Font.PLAIN, 20);
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// selectMood = 0;
-				frm.dispose();
-			}
-			
-		});
-		
-		// good
-		btn[1].addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// selectMood = 1;
-				
-				frm.dispose();				
-			}
-			
-		});
-		
-		// tired
-		btn[2].addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				frm.dispose();
-				
-				// selectMood = 2;
-			}
-			
-		});
-		
-		// bad
-		btn[3].addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				frm.dispose();
-				
-				// selectMood = 3;
-			}
-			
-		});
-		
-		// sad
-		btn[4].addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				frm.dispose();
-				
-				// selectMood = 4;
-			}
-			
-		});
-		
-		// sick
-		btn[5].addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// selectMood = 5;
-				
-				frm.dispose();
-			}
-			
-		});
-		
-		
-		frm.setVisible(true);
-	    
+        JFrame frm = new JFrame("Emotion");
+        frm.setSize(500, 400);
+        frm.setLocationRelativeTo(null);
+        frm.getContentPane().setLayout(null);
+        frm.setResizable(false);
+        frm.getContentPane().setBackground(Color.WHITE);
+        JLabel lab = new JLabel();
+        lab.setBounds(170,20,500,50);
+        lab.setText("Emotion selection");
+        lab.setFont(font);
+        frm.getContentPane().add(lab);
+        JButton [] btn = {new JButton(), new JButton(), new JButton(), new JButton(), new JButton(), new JButton()};
+        
+        int x = 75;
+        int y = 100;
+        for(int i = 0; i < btn.length; i++) {
+        btn[i].setBounds(x, y, 100, 100);
+            btn[i].setBorderPainted(false);
+            btn[i].setContentAreaFilled(false);
+            frm.getContentPane().add(btn[i]);
+            x += 130;
+            if(i % 3 == 2) {
+                y += 120;
+                x = 75;
+            }
+        }
+        
+        ImageIcon[] img = { new ImageIcon("./data/images/mood/happy.png"), new ImageIcon("./data/images/mood/good.png"), new ImageIcon("./data/images/mood/tired.png"),
+        new ImageIcon("./data/images/mood/bad.png"), new ImageIcon("./data/images/mood/sad.png"), new ImageIcon("./data/images/mood/sick.png") };
+        
+        for(int i = 0; i < img.length; i++) {
+            Image temp = img[i].getImage();
+            Image change = temp.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+            ImageIcon tempicon = new ImageIcon(change);
+            btn[i].setIcon(tempicon);
+        }
+        
+        // happy (0)
+        btn[0].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                try {
+                    File file = new File("./data/moodDB/" + day +".txt");
+                    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+                    if(file.isFile() && file.canWrite()) {
+                        bufferedWriter.write("0");
+                        bufferedWriter.close();
+                    }
+                }
+                catch (IOException e) {
+                    System.out.println(e);
+                }
+                frm.dispose();
+            }
+        });
+        
+        // good (1)
+        btn[1].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                try {
+                    File file = new File("./data/moodDB/" + day +".txt");
+                    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+                    if(file.isFile() && file.canWrite()) {
+                        bufferedWriter.write("1");
+                        bufferedWriter.close();
+                    }
+                }
+                catch (IOException e) {
+                    System.out.println(e);
+                }
+                frm.dispose();
+            }
+        });
+        
+        // tired (2)
+        btn[2].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                try {
+                    File file = new File("./data/moodDB/" + day +".txt");
+                    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+                    if(file.isFile() && file.canWrite()) {
+                        bufferedWriter.write("2");
+                        bufferedWriter.close();
+                    }
+                }
+                catch (IOException e) {
+                    System.out.println(e);
+                }
+                frm.dispose();
+            }
+        });
+        
+        // bad (3)
+        btn[3].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                try {
+                    File file = new File("./data/moodDB/" + day +".txt");
+                    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+                    if(file.isFile() && file.canWrite()) {
+                        bufferedWriter.write("3");
+                        bufferedWriter.close();
+                    }
+                }
+                catch (IOException e) {
+                    System.out.println(e);
+                }
+                frm.dispose();
+            }
+        });
+        
+        // sad (4)
+        btn[4].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                try {
+                    File file = new File("./data/moodDB/" + day +".txt");
+                    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+                    if(file.isFile() && file.canWrite()) {
+                        bufferedWriter.write("4");
+                        bufferedWriter.close();
+                    }
+                }
+                catch (IOException e) {
+                    System.out.println(e);
+                }
+                frm.dispose();
+            }
+        });
+        
+        // sick (5)
+        btn[5].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                try {
+                    File file = new File("./data/moodDB/" + day +".txt");
+                    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+                    if(file.isFile() && file.canWrite()) {
+                        bufferedWriter.write("5");
+                        bufferedWriter.close();
+                    }
+                }
+                catch (IOException e) {
+                    System.out.println(e);
+                }
+                frm.dispose();
+            }
+        });
+        
+        
+        frm.setVisible(true);
 	}
 	
 }
